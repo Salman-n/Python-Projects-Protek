d6dfedfca79d536d181b79cc8d984e7a072b6be7
Salman Wiryansyah <71618052+Salman-n@users.noreply.github.com>
2020-11-08 00:19:40 +0700

    Add files via upload

--- Tambahan Pengayaan/des2bin.py
+++ Tambahan Pengayaan/des2bin.py
@@ -1,4 +1,4 @@
-n = int(input("Enter a decimal number: "))
+n = int(input("Silahkan Masukan bilangan / angka yang anda mau: "))
 binary = ''
 rem = 0
 def Dec2bin(n):
@@ -9,7 +9,9 @@ def Dec2bin(n):
         if n > 1:
             Dec2bin(n // 2)
         print(rem, end= '')
-        binary = str(rem) + binary  
+        binary = str(rem) + binary
+        
+print("<<-------------------------->>")      
 print("Konversi ke binernya: ", binary)
 
 def Dec2bin(n):    
