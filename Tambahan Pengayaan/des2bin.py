2f291c70ba987d532b29a73cbf46803b3815db4f
Salman Wiryansyah <71618052+Salman-n@users.noreply.github.com>
2020-11-08 00:13:23 +0700

    Add files via upload

---
+++ Tambahan Pengayaan/des2bin.py
@@ -0,0 +1,29 @@
+n = int(input("Enter a decimal number: "))
+binary = ''
+rem = 0
+def Dec2bin(n):
+    
+    while True:
+        
+        rem = n % 2
+        if n > 1:
+            Dec2bin(n // 2)
+        print(rem, end= '')
+        binary = str(rem) + binary
+        
+        
+print("Konversi ke binernya: ", binary)
+
+def Dec2bin(n): 
+      
+    if n > 1: 
+        Dec2bin(n // 2) 
+    print(n % 2, end = '') 
+  
+if __name__ == "__main__": 
+      
+    # decimal value 
+    des = n
+      
+    # Calling function 
+    Dec2bin(des) 
