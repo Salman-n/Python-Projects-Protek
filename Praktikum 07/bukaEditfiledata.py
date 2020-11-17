42cdd1deeb9cb9a279329627056cde3797bb70d6
Salman Wiryansyah <71618052+Salman-n@users.noreply.github.com>
2020-11-17 20:12:53 +0700

    Add files via upload

---
+++ Praktikum 07/bukaEditfiledata.py
@@ -0,0 +1,16 @@
+namaDariFile = str(input("Silahkan anda masukan nama file : "))
+try :
+        while True :
+            filenya = open(namaDariFile, "a")
+            datanya = input("Data yang ingin ditambahkan kembali (y/n) ? :")
+            filenya.write(datanya +"\n")
+            filenya.close()
+            ulangi = input("Mau Lagi (y/n) ? : ")
+            if ulangi == "n":
+               break
+            if ulangi != "y":
+               print("Data tidak valid broo")
+               break
+except FileNotFoundError:
+    print("Terjadi kesalahan, File data tidak ditemukan Broo")
+    
