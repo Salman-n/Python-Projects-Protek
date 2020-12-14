bf92f7ed588e1cd587bab16c29babfa595571f71
Salman Wiryansyah <71618052+Salman-n@users.noreply.github.com>
2020-12-14 12:16:19 +0700

    Add files via upload

--- Praktikum 09/ubahHuruf.py
+++ Praktikum 09/ubahHuruf.py
@@ -1,10 +1,28 @@
+print("Cara Pertama :")
 def UbahHurufdariKata(str1, a, b):
+    print("Hasil Ubah huruf dalam kata", str1)
     Strng = str1 
     for katadalamstr1 in range(len(a)):
           ubah = Strng.replace(a[katadalamstr1],b[katadalamstr1])
           print("-"*25)
-          print('Hasil Ubah Huruf dalam Kata', str1, 'adalah')
           print(ubah)
           print("-"*25)
           
 UbahHurufdariKata("MATEMATIKA", "T", "S")
+print("")
+print("Cara Kedua :")
+def UbahHurufdariKata(str1, a, b):
+    print("Hasil Ubah huruf dalam kata", str1)
+    stringbaru = ""
+    for ubah in str1:
+        if ubah == a:
+            stringbaru = stringbaru + b
+        else:
+            stringbaru = stringbaru + ubah
+    print("-"*25)
+    print(stringbaru)
+    print("-"*25)
+          
+UbahHurufdariKata("MATEMATIKA", "T", "S")
+
+
