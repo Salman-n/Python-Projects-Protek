c19e3496df6da1255b7790489c715c567024578f
Salman Wiryansyah <71618052+Salman-n@users.noreply.github.com>
2020-12-30 17:40:52 +0700

    Add files via upload

--- Praktikum 10/praktikumX/3.py
+++ Praktikum 10/praktikumX/3.py
@@ -3,18 +3,46 @@ def program():
     
     Lines = file1.readlines() 
     
-      
+    char = {
+        1:'a',
+        2:'b',
+        3:'c',
+        4:'d',
+        5:'e',
+        6:'f',
+        7:'g',
+        8:'h',
+        9:'i',
+        10:'j',
+        11:'k',
+        12:'l',
+        13:'m',
+        14:'n',
+        15:'o',
+        16:'p',
+        17:'q',
+        18:'r',
+        19:'s',
+        20:'t',
+        21:'u',
+        22:'v',
+        23:'w',
+        24:'x',
+        25:'y',
+        26:'z',
+    }
     
-    data = []
-    # Strips the newline character 
-    
+    data = {}
+    # Strips Garis karakter baru
+    y = 0
     for line in Lines: 
+        y = y + 1
         x = line.strip().split("|",3)
         dict = {}
         dict["nim"] = x[1]
         dict["nama"] = x[2].replace("\t","")
         dict["alamat"] = x[3]
-        data.append(dict)
+        data[char[y]] = dict
         
     print(data)
 program()
